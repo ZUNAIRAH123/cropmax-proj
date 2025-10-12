116018c167b1673b8ce3666d030b80f2e85e2239
RichMan <truerichman@gmail.com>
2025-10-12 12:40:01 +0500

    rich

--- src/Components/Common/Footer.jsx
+++ src/Components/Common/Footer.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import cropmxLogo from "/cropmx__logo-removebg-preview.png";
-import { FaFacebookF, FaInstagram, FaWhatsapp, FaYoutube, FaPhoneAlt, FaClock, FaMapMarkerAlt } from "react-icons/fa";
+import {
+  FaFacebookF,
+  FaInstagram,
+  FaWhatsapp,
+  FaYoutube,
+  FaPhoneAlt,
+  FaClock,
+  FaMapMarkerAlt,
+} from "react-icons/fa";
 
 function Footer() {
   return (
@@ -35,14 +43,13 @@ function Footer() {
       {/* Middle Section */}
       <div className="max-w-7xl mx-auto px-6 py-10 grid grid-cols-1 md:grid-cols-4 gap-10 text-center md:text-left ">
         {/* Logo */}
-      <div className="flex justify-center md:justify-start">
-  <img
-    src={cropmxLogo}
-    alt="Company Logo"
-    className="h-32 object-contain" // h-20 → h-28
-  />
-</div>
-
+        <div className="flex justify-center md:justify-start">
+          <img
+            src={cropmxLogo}
+            alt="Company Logo"
+            className="h-32 object-contain" // h-20 → h-28
+          />
+        </div>
 
         {/* Categories */}
         <div>
@@ -67,9 +74,7 @@ function Footer() {
           <ul className="space-y-2 text-sm">
             <li>www.cscv.pk</li>
             <li>imdadseed@gmail.com</li>
-            <li>facebook.com/chatthaseed
-</li>
-            
+            <li>facebook.com/chatthaseed</li>
           </ul>
         </div>
 
@@ -81,24 +86,33 @@ function Footer() {
             <li>+92 300 7721318</li>
             <li>+92 300 8990123</li>
             <li>Support: 24/7</li>
-             <li>36 – H, Club Road, Vehari 61100 (Punjab) Pakistan</li>
- 
+            <li>36 – H, Club Road, Vehari 61100 (Punjab) Pakistan</li>
           </ul>
         </div>
-      </div>   
-        {/* Social Icons */}
-        <div className="border-t max-w-7xl mx-auto px-6 py-6 flex flex-col md:flex-row justify-between items-center ">
-  <div className="flex space-x-4 text-xl">
-    <a href="#"><FaFacebookF className="text-[#76B74E]" /></a>
-    <a href="#"><FaInstagram className="text-[#76B74E]" /></a>
-    <a href="#"><FaWhatsapp className="text-[#76B74E]" /></a>
-    <a href="#"><FaYoutube className="text-[#76B74E]" /></a>
-  </div>
+      </div>
+      {/* Social Icons */}
+      <div className="border-t max-w-7xl mx-auto px-6 py-6 flex flex-col md:flex-row justify-between items-center ">
+        <div className="flex space-x-4 text-xl">
+          <a href="#">
+            <FaFacebookF className="text-[#76B74E]" />
+          </a>
+          <a href="#">
+            <FaInstagram className="text-[#76B74E]" />
+          </a>
+          <a href="#">
+            <FaWhatsapp className="text-[#76B74E]" />
+          </a>
+          <a href="#">
+            <FaYoutube className="text-[#76B74E]" />
+          </a>
+        </div>
         {/* Copyright */}
         <p className="text-sm text-gray-500 mt-4 md:mt-0">
-         ©2025 ChattaSeed Corporation All rights reserved.
+          ©2025 ChattaSeed Corporation All rights reserved.
         </p>
-        <p>Powered By: ©BizCode</p>
+        <p>
+          <b>Powered By: ©BizCode</b>
+        </p>
       </div>
     </footer>
   );
